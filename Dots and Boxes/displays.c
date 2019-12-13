d022c18d61c1c5ee2b4a4574e81a234f0c6f94a8
seif gneedy <seifgneedy2@gmail.com>
2019-12-13 20:12:12 +0200

    display around the grid

--- Dots and Boxes/displays.c
+++ Dots and Boxes/displays.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include "displays.h"
+#include <time.h>
+int relines=0,score1,score2,move1,move2,starttime=0;
 /**********************************************************************
 * MainMenu: first clears the console then draw the main menu screen ***
 *         finally executing the user choice.                        ***
@@ -23,20 +24,20 @@ void MainMenu(){
            "                                3. Load a game.\n"
            "                                4. Top 10 players.\n"
            "                                \033[1;31m5. Exit.\n\033[0m"
-           "\n\n");
+           "\n\n        ");
     int choice = 0;
     for (;;){
-        printf("                                        ");
+        printf("\n\n enter your choice : ");
         scanf("%d",&choice);
         switch (choice){
             case 1:
-                gridSize = 5;
-                numOfBoxes = 2;
+                gridBoxes = 2;
+                relines=12;
                 ModeMenu();
                 break;
             case 2:
-                gridSize = 11;
-                numOfBoxes = 5;
+                gridBoxes = 5;
+                relines=60;
                 ModeMenu();
                 break;
             case 3:
@@ -81,10 +82,13 @@ void ModeMenu(){
                 switch(choice){
                 case 1:
                         numOfPlayers = 1;
+                        uicomp();
+                        //////////
                         /////////
                         break;
                     case 2:
                         numOfPlayers = 2;
+                        uiplayer();
                         ////////
                         ////////
                         break;
@@ -99,74 +103,114 @@ void ModeMenu(){
            }
 
 }
-void GridMenu(){
-    printf("         ");
-    for (int i=0; i<gridSize;i++){
-        printf("%d ",i+1);
-    }
-    for (int i=0; i<gridSize; i++){ // checks if i is 2 digits, So not to move rows of the grid
-        if (i/9){ // Notice i starts of 0
-            printf("\n       %d",i+1);
-        }
-        else{
-            printf("\n        %d",i+1);
-        }
-        for (int j=(i+1)%2; j<=numOfBoxes; j++){
-            if (i%2==0){
-                // This part is for dots and horizontal lines
-                dotShape();
-                if (grid[i][j] == true){
-                    switch (playerGrid[i][j]){
-                        case 1:
-                            blueL();
-                            Hline();
-                            break;
-                        case 2:
-                            redL();
-                            Hline();
-                            break;
-                    }
-                }
-                else {
-                    eHline();
-                }
-                if (j == numOfBoxes){
-                    dotShape();
-                }
-            }
-            // This part is for vertical lines and colored cells
-            else {
-                if (grid[i][j] == true){
-                    switch (playerGrid[i][j]){
-                        case 1:
-                            blueL();
-                            Vline();
-                            break;
-                        case 2:
-                            redL();
-                            Vline();
-                            break;
-                    }
-                }
-                else {
-                    eVline();
-                }
-                if (j != numOfBoxes){
-                    switch(boxesGrid[i/2][j]){
-                        case 1:
-                            blueB();
-                            break;
-                        case 2:
-                            redB();
-                            break;
-                        default :
-                            emptyB();
-                    }
-                }
-            }
-        }
+//function to display user interface if he plays with computer
+void uicomp(){
+    system("cls");
+    int m1,m2;
+    //time in minutes and seconds
+    int f;
+    time_t t1;
+    int starttime,mins,secs;
+    t1=time(NULL);
+    f=relines;
+    if (f==60||f==12){
+        starttime=t1;f--;}
+    t1=t1-starttime;
+    mins=t1/60;secs=t1%60;
+    //display in screen
+    printf("    TURN          REMAINING LINES          TIME   \n"
+      " \033[1;34m Player 1\033[0m              %d                 %d:%d  \n",relines,mins,secs);
+    printf("\033[1;33m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\033[0m\n");
+    printf("  \033[1;34mPlayer 1\033[0m                \033[31mCopmputer\033[0m     \n"
+           "  \033[1;34mMoves : %d \033[0m              \033[31mMoves : %d \033[0m     \n"
+           "  \033[1;34mScore : %d \033[0m              \033[31mScore : %d \033[0m     \n",move1,move2,score1,score2);
+           //here the grid
+           ///////////////////
+           //////////////////////////////////////////////////////////////////////////////////////////////////////
+    printf("press (50) undo ,(60) redo ,(70) save game ,(80) back to main menu ,(90) exit\n");
+    for (int i=0;i<relines;i++){
+    printf("enter your play :");
+    scanf("%d",&m1);
+    switch (m1){
+    case 50:
+        ///// undo operation /////
+        break;
+    case 60:
+        ///// redo operation /////
+        break;
+    case 70 :
+        ///// save game /////
+        break;
+    case 80 :
+        MainMenu();
+        break;
+    case 90 :
+        exit(EXIT_SUCCESS);
+    default :
+      scanf("%d",&m2);
+      ////////////////
+      break;
+
     }
 }
+}
+//function to display user interface if he plays with another player
+void uiplayer(){
+ system("cls");
+    int m1,m2;
+    //time in minutes and seconds
+    int f;
+    time_t t1;
+    int starttime,mins,secs;
+    t1=time(NULL);
+    f=relines;
+    if (f==60||f==12){
+        starttime=t1;f--;}
+    t1=t1-starttime;
+    mins=t1/60;secs=t1%60;
+    //display in screen
+    char playturn[9]="Player 1";
+
+
+    printf("    TURN          REMAINING LINES          TIME   \n"
+      " \033[1;34m %s\033[0m              %d                 %d:%d  \n",playturn,relines,mins,secs);
+    printf("\033[1;33m!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\033[0m\n");
+    printf("  \033[1;34mPlayer 1\033[0m                \033[31mPlayer 2\033[0m     \n"
+           "  \033[1;34mMoves : %d \033[0m              \033[31mMoves : %d \033[0m     \n"
+           "  \033[1;34mScore : %d \033[0m              \033[31mScore : %d \033[0m     \n",move1,move2,score1,score2);
+           //here the grid
+           ///////////////////
+           //////////////////////////////////////////////////////////////////////////////////////////////////////
+    printf("press (50) undo ,(60) redo ,(70) save game ,(80) back to main menu ,(90) exit\n");
+    for (int i=0;i<relines;i++){
+    printf("enter your play :");
+    scanf("%d",&m1);
+    switch (m1){
+    case 50:
+        ///// undo operation /////
+        break;
+    case 60:
+        ///// redo operation /////
+        break;
+    case 70 :
+        ///// save game /////
+        break;
+    case 80 :
+        MainMenu();
+        break;
+    case 90 :
+        exit(EXIT_SUCCESS);
+    default :
+      scanf("%d",&m2);
+      ////////////////
+      break;
+
+    }
+}
+}
+
+
+
 /**************************************
 * redL : every text after it is red ***
 **************************************/
@@ -177,25 +221,19 @@ void redL(){
 * redB : Background turn red ***
 *******************************/
 void redB(){
-    printf("\033[41m 2 \033[0m");
+    printf("\033[31m");
 }
 /****************************************
 * blueL : every text after it is blue ***
 ****************************************/
 void blueL(){
-    printf("\033[1;34m");
+    printf("\033[1;34");
 }
 /*********************************
 * blueB : Background turn blue ***
 *********************************/
 void blueB(){
-    printf("\033[44m 1 \033[0m");
-}
-/******************************
-* emptyB : empty background ***
-******************************/
-void emptyB(){
-    printf("   ");
+    printf("\033[34");
 }
 /******************************************************************
 * reset : remove the properties of text or background before it ***
@@ -211,12 +249,6 @@ void invalidInput(){
     printf("\n                                   invalid input          \n");
     reset();
 }
-/**************************************************************************
-*** dotShape : printing the dot shape so it can be changed at any time ***
-**************************************************************************/
-void dotShape(){
-    printf("*");
-}
 /*******************************************
 *** eHline : draws empty horizontal line ***
 *******************************************/
@@ -228,7 +260,6 @@ void eHline(){
 ************************************/
 void Hline(){
     printf("\304\304\304");
-    reset();
 }
 /*****************************************
 *** eVline : draws empty vertical line ***
@@ -241,5 +272,4 @@ void eVline(){
 **********************************/
 void Vline(){
     printf("\263");
-    reset();
 }
--- Dots and Boxes/displays.h
+++ Dots and Boxes/displays.h
@@ -1,19 +1,14 @@
 #ifndef DISPLAYS_H_INCLUDED
 #define DISPLAYS_H_INCLUDED
+
 void MainMenu();    void ModeMenu();
 void redL();        void redB();
 void blueL();       void blueB();
 void reset();
 void invalidInput();
-void dotShape();
-void emptyB();
 void eHline();     void Hline();
 void eVline();     void Vline();
 extern int numOfPlayers;
-extern int gridSize;
-extern int numOfBoxes;
-extern int records[60][4];
-extern bool grid[11][6];
-extern int playerGrid[11][11];
-extern int boxesGrid[5][5];
+extern int gridBoxes;
+
 #endif // DISPLAYS_H_INCLUDED
