e31a1bfc920278dc6872f25801ae1737685c5f32
seif gneedy <seifgneedy2@gmail.com>
2019-12-14 10:33:51 +0200

    Revert "display around the grid"
    This reverts commit d022c18d61c1c5ee2b4a4574e81a234f0c6f94a8.

--- Dots and Boxes/main.c
+++ Dots and Boxes/main.c
@@ -4,11 +4,14 @@
 #include "displays.h"
 #include "processing.h"
 int numOfPlayers; // 1 if against computer and 2 if two players
-int gridBoxes; // number of boxes on the grid
-
+int gridSize;    // The biggest number written above the grid
+int numOfBoxes; // number of boxes on the grid
+int records[60][4];
+bool grid[11][6];
+int playerGrid[11][11];
+int boxesGrid[5][5];
 int main()
 {
-
     MainMenu();
     exit(EXIT_SUCCESS);
 }
--- Dots and Boxes/processing.h
+++ Dots and Boxes/processing.h
@@ -2,5 +2,11 @@
 #define PROCESSING_H_INCLUDED
 
 
+extern int numOfPlayers;
+extern int gridSize;
+extern int records[60][4];
+extern bool grid[11][6];
+extern int playerGrid[11][11];
+extern int boxesGrid[5][5];
 
 #endif // PROCESSING_H_INCLUDED
